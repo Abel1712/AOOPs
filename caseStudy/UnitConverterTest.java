31e1211bbe6893a9dbf16e67b195374d983a0ec8
peter jackson <mpc171204@gmail.com>
2024-08-11 20:49:41 +0530

    modified:   caseStudy/UnitConverterTest.java

--- caseStudy/UnitConverterTest.java
+++ caseStudy/UnitConverterTest.java
@@ -6,27 +6,27 @@ public class UnitConverterTest {
     @Test
     public void testConvertLength() {
         // Test meter to kilometer
-        assertEquals(1.0, UnitConverter.convertLength(1000, "meter", "kilometer"), 0.001);
+        assertEquals(1.0, UnitConverter.convertLength(1000, "meter", "kilometer"));
         // Test kilometer to meter
-        assertEquals(1000.0, UnitConverter.convertLength(1, "kilometer", "meter"), 0.001);
+        assertEquals(1000.0, UnitConverter.convertLength(1, "kilometer", "meter"));
        
     }
 
     @Test
     public void testConvertWeight() {
         // Test kg to pound
-        assertEquals(2.20462, UnitConverter.convertWeight(1, "kg", "pound"), 0.00001);
+        assertEquals(2.20462, UnitConverter.convertWeight(1, "kg", "pound"));
         // Test pound to kg
-        assertEquals(1.0, UnitConverter.convertWeight(2.20462, "pound", "kg"), 0.00001);
+        assertEquals(1.0, UnitConverter.convertWeight(2.20462, "pound", "kg"));
       
     }
 
     @Test
     public void testConvertVolume() {
         // Test liter to milliliter
-        assertEquals(1000.0, UnitConverter.convertVolume(1, "liter", "milliliter"), 0.001);
+        assertEquals(1000.0, UnitConverter.convertVolume(1, "liter", "milliliter"));
         // Test milliliter to liter
-        assertEquals(1.0, UnitConverter.convertVolume(1000, "milliliter", "liter"), 0.001);
+        assertEquals(1.0, UnitConverter.convertVolume(1000, "milliliter", "liter"));
       
     }
 }
